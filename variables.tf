ed0e1de1caf650e1b19912731a4c3421cb51d5e0
Krish <krishpy3@gmail.com>
2022-05-28 00:54:09 +0530

    variable File updated

--- variables.tf
+++ variables.tf
@@ -1,18 +1,15 @@
 variable source_bucket {
   type        = string
-  default     = "mytestkrish"
   description = "Mention the bucket name. This will be used for storing the antivirus database and also it as a quarantine bucket"
 }
 
 variable prod_bucket {
   type        = string
-  default     = "krish-test-buc"
   description = "Mention the Prod bucket name so that the non-infected files will be moved to it"
 }
 
 variable sns_topic_arn {
   type        = string
-  default     = "arn:aws:sns:us-east-1:218063557524:iam-lambda-test"
   description = "SNS Topic ARN"
 }
 
